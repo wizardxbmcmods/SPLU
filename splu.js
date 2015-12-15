14b90e555b4f72cff106a1ecc249afe3c28ff0a7
wizardxbmcmods <wizardxbmcmods@gmail.com>
2015-12-15 10:02:35 -0600

    a.15

--- splu.js
+++ splu.js
@@ -2,7 +2,7 @@
 
     //Check if they aren't on a BGG site and alert them to that fact.
     if(window.location.host.slice(-17)!="boardgamegeek.com" &&  window.location.host.slice(-17)!="videogamegeek.com" && window.location.host.slice(-11)!="rpggeek.com" && window.location.host.slice(-6)!="bgg.cc" && window.location.host.slice(-10)!="geekdo.com"){
-      window.open("http://www.boardgamegeek.com", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=500, left=500, width=400, height=400");
+      window.open("http://www.boardgamegeek.com", "_parent", "toolbar=yes, scrollbars=yes, resizable=yes, top=500, left=500, width=400, height=400");
 	  window.focus();
 	  window.location.href("http://www.google.com");
 	  //window.location.href = "javascript:(function(){if(typeof SPLU=="undefined"){var SPLUscript=document.createElement('script');SPLUscript.type="text/javascript";SPLUscript.src='https://rawgit.com/wizardxbmcmods/SPLU/master/splu.js';document.body.appendChild(SPLUscript);}else{if(!document.getElementById('SPLUwindow')){initSPLU();}}})();";
